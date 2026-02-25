c96c055eb9f33314de5c73f81c62a63578439a8a
Andrey Nikolaenkov <nichexplore@gmail.com>
2026-02-25 17:44:58 +0300

    init

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 use DI\Container;
 
+session_start();
+
 $data = file_get_contents("user.json") ?: '{}';
 $users = json_decode($data, true);
 
@@ -14,6 +16,9 @@ $container = new Container();
 $container->set('renderer', function () {
     // Параметром передается базовая директория, в которой будут храниться шаблоны
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
 });
 
 $app = AppFactory::createFromContainer($container);
@@ -30,11 +35,13 @@ $app->get('/users', function ($request, $response) use ($users, $router) {
     $filtered = array_filter($users, function($user) use ($term) {
         return str_contains($user['nickname'], $term);
     });
+    $flash = $this->get('flash')->getMessages();
     $params = [
         'users' => $filtered,
         'term' => $term,
         'urlForUsers' => $router->urlFor('users'),
-        'urlForNew' => $router->urlFor('newUser')
+        'urlForNew' => $router->urlFor('newUser'),
+        'flash' => $flash
     ];
     return $this->get('renderer')->render($response, 'users/index.phtml', $params);
 })->setName('users');
@@ -51,7 +58,10 @@ $app->get('/users/{id:[0-9]+}', function ($request, $response, $args) use ($user
     }
 
     if ($user === null) {
-        return $response->withRedirect($router->urlFor('users'));
+        return $this->get('renderer')->render($response->withStatus(404), 'errors/404.phtml', [
+            'message' => '404: Пользователь не найден',
+            'url' => $router->urlFor('users')
+        ]);
     }
 
     $params = ['user' => $user];
@@ -68,7 +78,9 @@ $app->post('/users', function ($request, $response) use ($users, $router) {
     $user['id'] = count($users) + 1;
     $users[] = $user;
     file_put_contents('user.json', json_encode($users));
+    $this->get('flash')->addMessage('success', 'User was added successfully');
     return $response->withRedirect($router->urlFor('users'));
 })->setName('addUser');
 
 $app->run();
+
